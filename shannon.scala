742d9c5e47a3a83d55b57374725f50de3893c4de
tfarrell-vz <farrell@abiresearch.com>
2016-04-21 12:41:30 -0400

    Use groupBy to make a map and then apply map to make a list of values

--- shannon.scala
+++ shannon.scala
@@ -1,15 +1,11 @@
 def shannonEntropy(str: String): Double = {
-  val count = scala.collection.mutable.Map[Char, Double]()
-
-  for (s <- str) {
-    val currentCount = count.getOrElseUpdate(s, 0)
-    count.update(s, currentCount+1)
-  }
+  val counts = str.groupBy(c => c)
+                 .map(kv => kv._2.length)
 
   val len: Double = str.length
   (for {
-      v <- count.values
-      iter = (v / len) * scala.math.log(v/len) / scala.math.log(2)
+      count <- counts
+      iter = (count / len) * scala.math.log(count/len) / scala.math.log(2)
     } yield iter)
     .sum * (-1)
 }
